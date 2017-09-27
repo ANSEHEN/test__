5533dfc5c8d396adcf88c9e5bccd70ddb6b318a4
sungkyun88 <tasung08@gmail.co>
2017-09-27 01:45:18 +0000

    led on/off

--- led_run.c
+++ led_run.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <wiringPi.h>
-
+#define LED0 1 //gpio 18
 int main()
 {
+	wiringPiSetup();
+	pinMode(LED0,OUTPUT);
+	
+	while(1)
+	{
+		printf("1\n");
+		digitalWrite(LED0,1);
+		delay(7000);
+		printf("2\n");
+		digitalWrite(LED0,0);
+		delay(5000);
+	}
+	return 0;
+
+}
 
